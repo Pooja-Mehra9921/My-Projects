88ea6bba4117fe3c2e887301d46846e996240cd7
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2025-01-29 23:39:17 +0530

    making homepage header

---
+++ Amazon-Clone/src/component/Header/index.jsx
@@ -0,0 +1,83 @@
+import {
+  Box,
+  Button,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
+import FLIPKART_PLUS_IMAGE from "../../assents/images/FLIPKART_PLUS.png";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const userdata = JSON.parse(localStorage.getItem("userData"));
+  console.log("userdata------------", userdata);
+  const redirectToLogin = () => {
+    navigate("/login");
+  };
+
+  const redirectToHome = () => {
+    navigate("/home");
+  };
+  return (
+    <>
+      <Box className="header-main-container">
+        <Box className="header-container">
+          <Box className="flipkart-plus-image">
+            <img
+              onClick={redirectToHome}
+              className="flipkart-image-style"
+              src={FLIPKART_PLUS_IMAGE}
+              alt="flipkart image "
+            />
+          </Box>
+
+          <Box className="search-container">
+            <Box className="search-bar">
+              <Box className="search-icon">
+                <SearchIcon style={{ margin: "2px" }} />
+              </Box>
+              <TextField
+                style={{ width: "400px" }}
+                placeholder="Search for Products, Brands and More"
+                variant="standard"
+                size="normal"
+                InputProps={{ disableUnderline: true }}
+              />
+            </Box>
+            {
+                Boolean(userdata?.token)? 
+                <Button
+              className="login-btn"
+              variant="outlined"
+              onClick={redirectToLogin}
+            >
+              Login
+            </Button>
+            :
+            <Box className="user-profile">
+            <img src={userdata.image} alt="user image" />
+            <Typography>{userdata.firstName}</Typography>
+          </Box>
+            }
+            
+           
+            
+          </Box>
+          <Box className="add-to-cart">
+            <Typography className="cart">
+              {<ShoppingCartIcon style={{ fontSize: "30px" }} />} Cart
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
--- Amazon-Clone/src/pages/HomePage/index.jsx
+++ Amazon-Clone/src/pages/HomePage/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./style.css";
+import Header from "../../component/Header";
 
 const HomePage =()=>{
     return(
         <>
+        <Header/>
         
         This is a Home Page</>
     )
