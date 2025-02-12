10f1bd10b04c7acc8c385f7e4a746f33d8b52fc1
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2025-02-12 23:26:47 +0530

    adding image over effects

--- Amazon-Clone/src/pages/SingleProductDetail/index.jsx
+++ Amazon-Clone/src/pages/SingleProductDetail/index.jsx
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import ReactImageMagnify from "react-image-magnify";
+
+// import custom components
 import Header from "../../component/Header";
+import Footer from "../../component/Footer";
+
+// import material ui component
 import { Box } from "@mui/material";
 
-
 // import styles
-import "./style.css"
+import "./style.css";
 
-const SingleProductDetail = ()=>{
-    return(
-        <>
-        <Header/>
-        <Box className="pro-detail-container">
-            <Box className="pro-image-section">
-                image
-            </Box>
-            <Box className="pro-detail-section">
-                detail
-            </Box>
+const SingleProductDetail = () => {
+  const dataFromStore = useSelector((store) => store?.app?.selectedproduct);
+  console.log("data from store", dataFromStore);
 
+  const [imageToMagnify, setImageToMagnify] = useState(
+    dataFromStore?.thumbnail
+  );
+
+  const handleMainImageChange = (image) => {
+    setImageToMagnify(image);
+  };
+
+  return (
+    <>
+      <Header />
+      <Box className="pro-detail-container">
+        <Box className="pro-image-section">
+          <Box className="image-suggestions">
+            {dataFromStore?.images?.map((image, index) => (
+              <img
+                src={image}
+                alt={`Product ${index}`}
+                key={index}
+                className="img-suggestion"
+                onMouseOver={() => handleMainImageChange(image)} // ✅ Fixed
+              />
+            ))}
+          </Box>
+          <Box className="image-magnify-sec">
+            <ReactImageMagnify
+              className="thumbnail"
+              {...{
+                smallImage: {
+                  alt: "Product Image",
+                  isFluidWidth: true,
+                  src: imageToMagnify, // ✅ Fixed
+                },
+                largeImage: {
+                  src: imageToMagnify, // ✅ Fixed
+                  width: 1200,
+                  height: 1800,
+                  className: "large-img",
+                },
+                isHintEnabled: true,
+                enlargedImageContainerDimensions: {
+                  width: "100%",
+                  height: "100%",
+                },
+              }}
+            />
+          </Box>
         </Box>
-        </>
-    )
+        <Box className="pro-detail-section">detail</Box>
+      </Box>
+      <Footer />
+    </>
+  );
 };
 
 export default SingleProductDetail;
