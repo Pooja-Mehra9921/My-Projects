827c8115bb2dc24c259e4421c328882a3c4d0d51
Pooja-Mehra9921 <io6.poojamehra@gmail.com>
2025-02-13 17:59:51 +0530

    making image magnify section

--- Amazon-Clone/src/pages/SingleProductDetail/index.jsx
+++ Amazon-Clone/src/pages/SingleProductDetail/index.jsx
@@ -1,24 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import ReactImageMagnify from "react-image-magnify";
 
 // import custom components
 import Header from "../../component/Header";
 import Footer from "../../component/Footer";
 
 // import material ui component
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import StarIcon from "@mui/icons-material/Star";
 
 // import styles
 import "./style.css";
+import ReactImageMagnify from "react-image-magnify";
+import { DollarToIndianPrice, GetDiscountFromPrice } from "../../utility";
+import axios from "axios";
+import { API } from "../../configs/api";
 
 const SingleProductDetail = () => {
   const dataFromStore = useSelector((store) => store?.app?.selectedproduct);
-  console.log("data from store", dataFromStore);
 
-  const [imageToMagnify, setImageToMagnify] = useState(
-    dataFromStore?.thumbnail
-  );
+  const [imageToMagnify, setImageToMagnify] = useState(dataFromStore?.thumbnail);
+
+
+  const fetchSelectedProduct =async()=>{
+    const response = await axios.get(API.PRODUCT_BY_ID);
+    console.log("product bt id", response);
+  }
+
+  useEffect(() => {
+    if (dataFromStore) {
+      setImageToMagnify(dataFromStore.thumbnail);
+    fetchSelectedProduct();
+
+    }
+
+  }, [dataFromStore]);
+
+  if (!dataFromStore) {
+    return <p>Loading product details...</p>;
+  }
+
+/**
+ * @description function to set product images in image magnify
+ * @param {number} image 
+ */
 
   const handleMainImageChange = (image) => {
     setImageToMagnify(image);
@@ -28,6 +55,7 @@ const SingleProductDetail = () => {
     <>
       <Header />
       <Box className="pro-detail-container">
+        {/*images section*/}
         <Box className="pro-image-section">
           <Box className="image-suggestions">
             {dataFromStore?.images?.map((image, index) => (
@@ -36,35 +64,117 @@ const SingleProductDetail = () => {
                 alt={`Product ${index}`}
                 key={index}
                 className="img-suggestion"
-                onMouseOver={() => handleMainImageChange(image)} // ✅ Fixed
+                onMouseOver={() => handleMainImageChange(image)}
               />
             ))}
           </Box>
           <Box className="image-magnify-sec">
-            <ReactImageMagnify
-              className="thumbnail"
-              {...{
-                smallImage: {
-                  alt: "Product Image",
-                  isFluidWidth: true,
-                  src: imageToMagnify, // ✅ Fixed
-                },
-                largeImage: {
-                  src: imageToMagnify, // ✅ Fixed
-                  width: 1200,
-                  height: 1800,
-                  className: "large-img",
-                },
-                isHintEnabled: true,
-                enlargedImageContainerDimensions: {
-                  width: "100%",
-                  height: "100%",
-                },
-              }}
-            />
+            <Box className="magnify-image">
+              <ReactImageMagnify
+                {...{
+                  smallImage: {
+                    alt: "Wristwatch by Ted Baker London",
+                    isFluidWidth: true,
+                    src: imageToMagnify,
+                  },
+                  largeImage: {
+                    src: imageToMagnify,
+                    width: 1000,
+                    height: 1000,
+                  },
+                  enlargedImageContainerStyle: {
+                    zIndex: "1500",
+                  },
+                  enlargedImageContainerDimensions: {
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "#000000",
+                  },
+                }}
+              />
+            </Box>
+            <Box className="btn-container">
+              <Button
+                variant="contained  "
+                style={{
+                  color: "white",
+                  margin: "5px",
+                  backgroundColor: "#ff9f00",
+                  border: "none",
+                }}
+              >
+                <ShoppingCartIcon />
+                Add to Cart
+              </Button>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "#fb641b", margin: "5px" }}
+              >
+                <FlashOnIcon />
+                Buy Now
+              </Button>
+            </Box>
           </Box>
         </Box>
-        <Box className="pro-detail-section">detail</Box>
+
+        {/*Detail section*/}
+
+        <Box className="pro-detail-section">
+          <Typography variant="h5">{dataFromStore.title}</Typography>
+          <Typography variant="body1">{dataFromStore.description}</Typography>
+          {/*Price section*/}
+          <Box className="selected-price-section">
+            <Box style={{ display: "flex", margin: "10px auto" }}>
+              <Button className="rating-btn" variant="contained">
+                {Number(dataFromStore?.rating).toFixed(1)}
+                <StarIcon style={{ fontSize: "16px", marginTop: "-3px" }} />
+              </Button>
+              <Typography style={{ color: "grey" }}>{`${
+                dataFromStore?.rating
+              } Ratings & ${
+                (dataFromStore?.reviews).length || 0
+              } Reviews`}</Typography>
+            </Box>
+            <Box className="price-container">
+              <Typography className="selected-price">
+                &#8377;{DollarToIndianPrice(dataFromStore?.price)}
+              </Typography>
+              <Typography className="selected-orignal-price">
+                &#8377;
+                {GetDiscountFromPrice(
+                  dataFromStore?.price,
+                  dataFromStore?.discountPercentage
+                )}
+              </Typography>
+              <Typography className="selected-discount-price">{`${dataFromStore?.discountPercentage} % off`}</Typography>
+            </Box>
+
+            <Typography
+              sx={{
+                color:
+                  dataFromStore?.availabilityStatus === "In Stock"
+                    ? "green"
+                    : "red",
+              }}
+            >
+              {dataFromStore?.availabilityStatus}
+            </Typography>
+            <Typography>{dataFromStore?.warrantyInformation}</Typography>
+            <Typography>{dataFromStore?.returnPolicy}</Typography>
+            <Typography>{dataFromStore?.shippingInformation}</Typography>
+            <Typography>{dataFromStore?.stock}</Typography>
+            <Typography>{dataFromStore?.warrantyInformation}</Typography>
+          </Box>
+
+          <Box className="review-rating-section">
+            <Typography variant="h5">Ratings & Reviews</Typography>
+            <Box className="review-left-section">
+
+            </Box>
+            <Box className="review-right-section"></Box>
+
+          </Box>
+        </Box>
       </Box>
       <Footer />
     </>
